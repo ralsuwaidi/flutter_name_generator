95167e87717701cb7842df2412927d1ebf3a5308
rashed <x.suwaidi@gmail.com>
2020-08-23 17:05:58 +0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,308 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart'; // Add this line.
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      theme: ThemeData.dark(),
+      home: RedditWritingPrompts(),
+    );
+  }
+}
+
+class RedditWritingPrompts extends StatefulWidget {
+  @override
+  _RedditWritingPromptsState createState() => _RedditWritingPromptsState();
+}
+
+class _RedditWritingPromptsState extends State<RedditWritingPrompts> {
+  var _redditList = List<RedditPost>();
+  String _period = 'week';
+  @override
+  void initState() {
+    _populateWP(_period);
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[850],
+        title: Text('  Writing Prompts: ' +
+            _period.replaceFirst(_period[0], _period[0].toUpperCase())),
+        actions: [
+          PopupMenuButton<int>(
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                value: 1,
+                child: Text("Daily"),
+              ),
+              PopupMenuItem(
+                value: 2,
+                child: Text("Weekly"),
+              ),
+              PopupMenuItem(
+                value: 3,
+                child: Text("Monthly"),
+              ),
+            ],
+            onSelected: (value) {
+              if (value == 1) {
+                _period = 'day';
+              }
+              if (value == 2) {
+                _period = 'week';
+              }
+              if (value == 3) {
+                _period = 'month';
+              }
+              _select(_period);
+              Fluttertoast.showToast(
+                  msg: 'Top of the ' + _period,
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.BOTTOM,
+                  timeInSecForIosWeb: 1,
+                  backgroundColor: Colors.red,
+                  textColor: Colors.white,
+                  fontSize: 16.0);
+            },
+          )
+        ],
+      ),
+      body: _buildWPList(_period),
+    );
+  }
+
+  // build list of stories
+  Widget _buildWPList(String period) {
+    return FutureBuilder(
+      future: _response(period),
+      builder: (_context, snapshot) {
+        return Scrollbar(
+            child: ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: _redditList.length * 2,
+                itemBuilder: (BuildContext _context, int i) {
+                  if (i.isOdd) {
+                    return Divider();
+                  }
+                  final int index = i ~/ 2;
+
+                  return _buildRow(_redditList[index], index);
+                }));
+      },
+    );
+  }
+
+  // story after it is clicked
+  Widget _buildStory(String url) {
+    final theStory = new RedditPost()._getStory(url);
+    return FutureBuilder(
+      future: theStory,
+      builder: (_context, snapshot) {
+        if (snapshot.hasData) {
+          return NotificationListener<OverscrollIndicatorNotification>(
+              onNotification: (overscroll) {
+                overscroll.disallowGlow();
+                return null;
+              },
+              child: Scrollbar(
+                  child: Markdown(
+                data: snapshot.data,
+                styleSheet: MarkdownStyleSheet(p: TextStyle(fontSize: 16)),
+              )));
+        }
+
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+
+  Widget _buildRow(RedditPost post, index) {
+    List<Icon> _awardList(int awards) {
+      List<Icon> _iconList = List<Icon>();
+
+      for (var i = 0; i < awards; i++) {
+        _iconList.add(Icon(Icons.stars, size: 18, color: Colors.yellow));
+      }
+
+      return _iconList;
+    }
+
+    return Container(
+        child: ListTile(
+      title: Text(
+        post.title,
+      ),
+      subtitle: Column(
+        children: <Widget>[
+          if (post.awards != 0) SizedBox(height: 5),
+          Row(
+            children: _awardList(post.awards),
+          )
+        ],
+        crossAxisAlignment: CrossAxisAlignment.start,
+      ),
+      onTap: () {
+        _pushSaved(index);
+      },
+    ));
+  }
+
+  void _pushSaved(index) {
+    Navigator.of(context).push(MaterialPageRoute<void>(
+      builder: (BuildContext context) {
+        return Scaffold(
+          // appBar: AppBar(
+          //   title: Text("Story"),
+          // ),
+          // body: _buildStory(_redditList[index].url),
+          body: NestedScrollView(
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  expandedHeight: 200.0,
+                  floating: false,
+                  pinned: true,
+                  backgroundColor: Colors.grey[850],
+                  flexibleSpace: FlexibleSpaceBar(
+                    background: Container(
+                        margin: const EdgeInsets.fromLTRB(15, 80, 15, 9),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              _redditList[index].title,
+                              style: TextStyle(fontSize: 14),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                RichText(
+                                  text: TextSpan(
+                                      text: 'Score: ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                      children: <TextSpan>[
+                                        TextSpan(
+                                            text: _redditList[index]
+                                                .score
+                                                .toString(),
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.normal)),
+                                      ]),
+                                ),
+                                RichText(
+                                  text: TextSpan(
+                                      text: 'Date: ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                      children: <TextSpan>[
+                                        TextSpan(
+                                            text: DateFormat('dd-MM-yyyy ')
+                                                .format(DateTime
+                                                    .fromMillisecondsSinceEpoch(
+                                                        _redditList[index]
+                                                                .date
+                                                                .toInt() *
+                                                            1000))
+                                                .toString(),
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.normal)),
+                                      ]),
+                                ),
+                              ],
+                            ),
+                          ],
+                        )),
+                  ),
+                ),
+              ];
+            },
+            body: Center(
+              child: _buildStory(_redditList[index].url),
+            ),
+          ),
+        );
+      },
+    ));
+  }
+
+  void _select(String period) {
+    setState(() {
+      _populateWP(period);
+    });
+  }
+
+  Future _response(String period) {
+    if (period == 'week') {
+      return http
+          .get('https://www.reddit.com/r/WritingPrompts/top/.json?t=week');
+    }
+    if (period == 'day') {
+      return http
+          .get('https://www.reddit.com/r/WritingPrompts/top/.json?t=day');
+    }
+    return http
+        .get('https://www.reddit.com/r/WritingPrompts/top/.json?t=month');
+  }
+
+  _populateWP(period) async {
+    final responseresult = await _response(period);
+    final List posts = jsonDecode(responseresult.body)['data']['children'];
+    final titleList = posts.map((e) => e['data']['title']).toList();
+    final urlList = posts.map((e) => e['data']['url']).toList();
+    final dateList = posts.map((e) => e['data']['created']).toList();
+    final scoreList = posts.map((e) => e['data']['ups']).toList();
+    final awaredList =
+        posts.map((e) => e['data']['total_awards_received']).toList();
+
+    // make new list of RedditPost with url and title
+    _redditList = new List<RedditPost>();
+    for (var i = 0; i < titleList.length; i++) {
+      var newPost = RedditPost(
+          title: titleList[i],
+          url: urlList[i],
+          awards: awaredList[i],
+          score: scoreList[i],
+          date: dateList[i]);
+      _redditList.add(newPost);
+    }
+  }
+}
+
+class RedditPost {
+  const RedditPost({this.title, this.url, this.awards, this.date, this.score});
+
+  final String title;
+  final String url;
+  final int awards;
+  final int score;
+  final double date;
+
+  Future<String> _getStory(String url) async {
+    final _response = await http.get(url + '.json');
+    final String comment = jsonDecode(_response.body)[1]['data']['children'][1]
+            ['data']['body']
+        .toString();
+    return comment;
+  }
+}
